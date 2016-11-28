e2e62690d2ec337c7f9567f7229a254a25a55c2a
dy365cc <65941821@qq.com>
2016-11-28 16:50:58 +0800

    使用sqllite数据库并创建基本数据

--- zclg/myzcgl/models.py
+++ zclg/myzcgl/models.py
@@ -1,5 +1,12 @@
-from __future__ import unicode_literals
-
 from django.db import models
 
 # Create your models here.
+class userinfo(models.Model):
+    userID = models.CharField(max_length = 10)
+    name = models.CharField(max_length = 50)
+    pwd = models.CharField(max_length = 100)
+    userlevel = models.IntegerField()
+
+class xm_xmjbxx(models.Model):
+    xmbm = models.CharField(max_length = 50)
+    xmnc = models.CharField(max_length=20)
--- zclg/zclg/settings.py
+++ zclg/zclg/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myzcgl'
 ]
 
 MIDDLEWARE = [
@@ -103,15 +104,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh_Hans'#'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'#'UTC'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False#True
 
 
 # Static files (CSS, JavaScript, Images)
